9beeddf6f8ccc8ac8edaafe89fa191948d5f2fa3
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-15 01:10:05 +0000

    Avance 2

--- src/app.js
+++ src/app.js
@@ -6,12 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
-  const perfil = [
+  //Array de perfiles con nombres y ocupaciones
+  const perfiles = [
     { nombre: "Ana", ocupacion: "Maestra" },
     { nombre: "Carlos", ocupacion: "Diseñador" },
     { nombre: "Miguel", ocupacion: "Estudiante" },
-    { nombre: "Maria", ocupacion: "Desarrolladora" },
+    { nombre: "Maria", ocupacion: "Desarrolladora" }
   ];
+  //Evento de clic para generar un perfil aleatorio. Cuando escuche clic haga esto.
+  document.querySelector(".btn-generar").addEventListener("click", () => {
+    const perfil = perfiles[Math.floor(Math.random() * perfiles.length)]; //Entra a perfiles, toma un valor y que le genere uno de lo 4.
+  });
 };
