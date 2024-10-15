87c819738b16504849c301054df583da70a396a0
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-15 03:00:18 +0000

    Logica parte 1

--- src/app.js
+++ src/app.js
@@ -7,6 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //Información a poner aleatoria
+  const simbolos = ["♦", "♥", "♠", "♣"];
+  const numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+  ];
   //Evento de clic para generar un perfil aleatorio. Cuando escuche clic haga esto.
   document.querySelector(".btn-generar").addEventListener("click", () => {});
 };
